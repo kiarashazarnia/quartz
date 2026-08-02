46ddc819e4189665abd937a5748f7c27b4938855
kiarash <kiarash.azarnia@gmail.com>
2026-08-02 20:26:56 +0100

    chore: style

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,15 +1,27 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    // blog-style "recent posts" roll, only on the homepage
+    Component.ConditionalRender({
+      component: Component.RecentNotes({
+        title: "Recent Posts",
+        limit: 3,
+        linkToMore: "posts" as SimpleSlug,
+        showTags: true,
+        filter: (f) => f.slug !== undefined && f.slug.startsWith("posts/"),
+      }),
+      condition: (page) => page.fileData.slug === "index",
+    }),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/kiarashazarnia",
     },
   }),
 }
