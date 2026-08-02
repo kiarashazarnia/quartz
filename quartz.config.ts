b0f20f403b4af8c87157da1ecc0ac611f5e338f1
kiarash <kiarash.azarnia@gmail.com>
2026-08-02 23:22:14 +0100

    chore: style

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Montserrat",
+        body: "Roboto",
         code: "IBM Plex Mono",
       },
       colors: {
